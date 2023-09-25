4015d2683ee50cc039179f7ed1dc5e5e8f2607a7
jin-Rh <119802458+jin-Rh@users.noreply.github.com>
2023-09-25 14:57:03 +0200

    Update TestBFS.java
    Breadth-First Search algorithm is systematic search using the first-in first-out queue data structure. It checks a goal state when inserting the node and find a shortest path by iterating all neighghbours. The example code is implemented using graph version BFS using initial 8 nodes and each node has 3 neighbours.

--- TestBFS.java
+++ TestBFS.java
@@ -1,5 +1,3 @@
-package com.snow.BFS;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,14 +8,14 @@ public class TestBFS {
     public static void main(String[] args) {
 
         // Create new nodes
-        BFSNode one = new BFSNode(1);
-        BFSNode two = new BFSNode(2);
-        BFSNode three = new BFSNode(3);
-        BFSNode four = new BFSNode(4);
-        BFSNode five = new BFSNode(5);
-        BFSNode six = new BFSNode(6);
-        BFSNode seven = new BFSNode(7);
-        BFSNode eight = new BFSNode(8);
+        BFSNode one = new BFSNode(1, 0);
+        BFSNode two = new BFSNode(2, 0);
+        BFSNode three = new BFSNode(3, 0);
+        BFSNode four = new BFSNode(4, 0);
+        BFSNode five = new BFSNode(5, 0);
+        BFSNode six = new BFSNode(6, 0);
+        BFSNode seven = new BFSNode(7, 1);
+        BFSNode eight = new BFSNode(8, 1);
 
         // Add neighbours on each node
         one.addNeighbours(one);
@@ -30,6 +28,7 @@ public class TestBFS {
 
         three.addNeighbours(three);
         three.addNeighbours(four);
+        three.addNeighbours(three);
 
         four.addNeighbours(three);
         four.addNeighbours(four);
@@ -39,8 +38,9 @@ public class TestBFS {
         five.addNeighbours(six);
         five.addNeighbours(seven);
 
-        six.addNeighbours(six);
         six.addNeighbours(five);
+        six.addNeighbours(six);
+        six.addNeighbours(six);
 
         seven.addNeighbours(seven);
         seven.addNeighbours(eight);
@@ -53,24 +53,31 @@ public class TestBFS {
         traverseNeighbours(one);
 
     }
+
     // BFS traversal method
-    static void traverseNeighbours(BFSNode startNode){
+    static void traverseNeighbours(BFSNode startNode) {
 
         LinkedList<BFSNode> queue = new LinkedList<>(); // holds all nodes to traverse
         BFSNode currentNode = startNode;
         queue.add(currentNode);
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             // remove the current first node from the queue
             currentNode = queue.poll();
             currentNode.setVisited(); // set to 1
             // print out the recently removed node
-            System.out.println(currentNode);
+            System.out.println("Visiting " + currentNode);
+            // Check whether the goal state is reached and break
+            if (currentNode.isGoalState()) {
+                System.out.println("Goal reached " + currentNode);
+                break;
+            }
             // explore all neighbours
             for (BFSNode nextNode : currentNode.getNeighbours()) {
                 if (nextNode.isVisited() == 0) { //add unvisited nodes to the queue
                     nextNode.setVisited();
                     queue.add(nextNode);
+
                 }
             }
         }
@@ -82,16 +89,19 @@ class BFSNode {
     private int node;
     private List<BFSNode> neighbours;
     private int visited;
+    private int state;
 
-    public BFSNode(int node) {
+    public BFSNode(int node, int state) {
         this.node = node;  //node number
         this.neighbours = new ArrayList<>(); //neighbouring nodes
         this.visited = 0; // unvisited
+        this.state = state;
     }
 
-    public void addNeighbours(BFSNode node){
+    public void addNeighbours(BFSNode node) {
         this.neighbours.add(node);
     }
+
     public int getNode() {
         return node;
     }
@@ -106,6 +116,11 @@ class BFSNode {
 
     public List<BFSNode> getNeighbours() {
         return this.neighbours;
+    }
+
+    public boolean isGoalState() {
+        return this.state == 1;
+
     }
 
     @Override
